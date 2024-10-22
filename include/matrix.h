7e7c9656d4c6b73cc05085ed317a9e6073132742
Viksne Nikita <vixnenik@yandex.ru>
2024-10-22 18:58:45 +0300

    Нашел основную проблему, смешно, но на ошибки в большей степени влиял пересчет радиусов Земли. Без него обходиться нельзя, надо думать над компенсацией ошибок вычислений

--- include/matrix.h
+++ include/matrix.h
@@ -3,5 +3,6 @@
 
 void MulMatrD(double *, double *, double *, int , int , int);
 void MatrOB(double, double, double, double*, int );
+void print2dMatr(double* A, int size);
 
 #endif
--- matrix.cpp
+++ matrix.cpp
@@ -1,26 +1,35 @@
 #include <std.h>
 #include <cmath>
+#include "mathematics.h"
+#include <stdio.h>
+
 void MulMatrD(double *A, double *B, double *C, int size1, int size2, int size3)
 {
 	/*
-	A[size1*size2] @ B[size2*size3] = C[size1*size3]
-	*/
+	 * A[size1*size2] @ B[size2*size3] = C[size1*size3]
+	 */
 	for (int i=0; i<size1; ++i) // по строкам A
 		for(int j=0; j<size3; ++j) // по столбцам B
 		{
 			double sum=0;
+			double errProd=0;
+			double errSum=0;
 			for (int k=0; k<size2; ++k) // по столбцам A (по строкам B)
 			{
-#if 0
-				double a = A[index(size2, i, k)];
-				double b = B[index(size3, k, j)];
-#endif
-				sum += (double) A[index(size2, i, k)] * (B[index(size3, k, j)]);
+				#if 1
+				double tempRes=0;
+				// умножение
+				TwoProduct(A[index(size2, i, k)], B[index(size3, k, j)], tempRes, errProd);
+				// Сложение
+				TwoSum(sum, tempRes, sum, errSum, false);
+				// Компенсация накопленной ошибки
+				TwoSum(sum, errSum, sum, errSum, false);
+				#endif
+				//sum += (double) A[index(size2, i, k)] * (B[index(size3, k, j)]);
 			}
-			C[index(size3, i, j)] = (double) sum;
+			C[index(size3, i, j)] = sum;
 		}
 }
-
 void MatrOB(double H, double R, double P, double* C, int size)
 {
 	C[0] = (double) cos(R) * cos(H) + sin(R)*sin(H)* sin(P);
@@ -32,4 +41,14 @@ void MatrOB(double H, double R, double P, double* C, int size)
 	C[6] = (double) sin(R) * cos(H) - cos(R) * sin(H) * sin(P);
 	C[7] = (double) -sin(R) * sin(H) - cos(R) * cos(H) * sin(P);
 	C[8] = (double) cos(R) * cos(P);
+}
+
+void print2dMatr(double* A, int size)
+{
+	for(int iii=0; iii<size; ++iii)
+	{
+		for(int jjj=0; jjj<size; ++jjj)
+			printf("%.20f\t", A[index(size, iii,jjj)]);
+		printf("\n");
+	}
 }
