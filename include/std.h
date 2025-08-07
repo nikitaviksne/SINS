2c8aeda0d0a636972445ac6944e57ecb4c7d416e
Viksne Nikita <vixnenik@yandex.ru>
2025-08-07 21:31:42 +0300

    ывфывф

--- include/std.h
+++ include/std.h
@@ -1,19 +1,9 @@
-<<<<<<< Updated upstream
 /*Файл с define*/
-#ifndef STDH
-#define STDH
-
-#define index_3(cols, row, col) (row*cols + col)
-//#define index(cols, row, col) (row*cols + col)
-#define Ldoub double
-
-#endif
-=======
 #ifndef STD_H
 #define STD_H
 
+#define index_3(cols, row, col) (row*cols + col)
+#define index(cols, row, col) (row*cols + col)
 #define Ldoub double
 
-#define index(columns, iRow, jCol) columns*iRow + jCol 
 #endif
->>>>>>> Stashed changes
